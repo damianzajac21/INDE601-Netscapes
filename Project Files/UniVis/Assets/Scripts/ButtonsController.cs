b1c3d51d4a04be1188f5a54c4b00e8002e576ceb
(s) Damian Zajac <dgzajac@pm7-0086.uopnet.plymouth.ac.uk>
2019-11-07 17:04:17 +0000

    added new font, fixed the button

---
+++ Project Files/UniVis/Assets/Scripts/ButtonsController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsController : MonoBehaviour
+{
+  public void GotoElementsPage() {
+		SceneManager.LoadScene("ElementPage");
+	}
+}
